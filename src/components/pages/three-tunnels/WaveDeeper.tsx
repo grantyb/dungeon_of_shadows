5da71d5a7b28caf2b6c6a84a03a6345785e99e31
Luke Berridge <lukeberridge2016@gmail.com>
2025-12-28 14:57:44 +1300

    correct the route for opting out of the river

---
+++ src/components/pages/three-tunnels/WaveDeeper.tsx
@@ -0,0 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import img from "../../../assets/dungeon/tunnels/wave-symbol.png"
+import StandardPage from "../../StandardPage"
+import Conversation from "../../Conversation"
+
+export const WaveDeeper = () => {
+	const navigate = useNavigate()
+
+	return (
+		<StandardPage backgroundImage={img} title="The Three Tunnels">
+			<Conversation width={20}>
+				<p>You choose the tunnel marked with a wave.</p>
+				<p>
+					As you proceed, the sound of rushing water grows louder, and
+					soon you find yourself in a vast underground cavern filled
+					with a roaring river. The walls glisten with moisture, and
+					the air is cool and damp.
+				</p>
+				<p>
+					Do you wish to follow the river deeper into the cavern?
+					<div className="conversation-controls">
+						<button
+							tabIndex={0}
+							onClick={() => navigate("/tunnels/")}
+						>
+							No
+						</button>
+						<button
+							tabIndex={0}
+							onClick={() => navigate("/attack-wizard/")}
+						>
+							Yes
+						</button>
+					</div>
+				</p>
+			</Conversation>
+		</StandardPage>
+	)
+}
