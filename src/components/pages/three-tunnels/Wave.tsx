a5a409e32cc704f2b83d403f336bd6e8cd6452c4
Grant Berridge <grant@cactuslab.com>
2025-12-28 14:39:27 +1300

    Add wave tunnel image

--- src/components/pages/three-tunnels/Wave.tsx
+++ src/components/pages/three-tunnels/Wave.tsx
@@ -1,30 +1,34 @@
 import { useNavigate } from "react-router-dom";
-import img from "../../../assets/dungeon/greet-wizard.png";
-import StandardPage from "../../StandardPage";
-import Conversation from "../../Conversation";
+import img from "../../../assets/dungeon/tunnels/wave-symbol.png"
+import StandardPage from "../../StandardPage"
+import Conversation from "../../Conversation"
 
 export const Wave = () => {
 	const navigate = useNavigate()
 
 	return (
 		<StandardPage backgroundImage={img} title="The Three Tunnels">
-			<Conversation width={35}>
+			<Conversation width={20}>
+				<p>You choose the tunnel marked with a wave.</p>
 				<p>
-					You choose the tunnel marked with a wave.
-				</p>
-				<p>
-					As you proceed, the sound of rushing water grows louder,
-					and soon you find yourself in a vast underground cavern filled
+					As you proceed, the sound of rushing water grows louder, and
+					soon you find yourself in a vast underground cavern filled
 					with a roaring river. The walls glisten with moisture, and
 					the air is cool and damp.
 				</p>
 				<p>
-					Do you wish to follow the river deeper into the cavern,
+					Do you wish to follow the river deeper into the cavern?
 					<div className="conversation-controls">
-						<button tabIndex={0} onClick={() => navigate("/proceed/")}>
+						<button
+							tabIndex={0}
+							onClick={() => navigate("/proceed/")}
+						>
 							No
 						</button>
-						<button tabIndex={0} onClick={() => navigate("/attack-wizard/")}>
+						<button
+							tabIndex={0}
+							onClick={() => navigate("/attack-wizard/")}
+						>
 							Yes
 						</button>
 					</div>
