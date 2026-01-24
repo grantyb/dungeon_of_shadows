c862ef8e8398ece30f06331c4271b6f6b5c300c3
Luke Berridge <lukeberridge2016@gmail.com>
2026-01-25 11:06:27 +1300

    fix combat controls

--- src/components/Combat.tsx
+++ src/components/Combat.tsx
@@ -18,14 +18,15 @@ const Combat: React.FC<CombatProps> = ({ foe, backgroundImage, children }) => {
 			<div className="main-title">
 				<h1>Combat with {foe}</h1>
 			</div>
-			<Conversation controls={false} width={10}>
+			<Conversation width={20}>
 				<p>You are now in combat with {foe}!</p>
-				<p>Choose your action:</p>
-				
-			<Button onClick={() => alert("Combat system coming soon!")} label={"dash"}></Button>
-			<Button onClick={() => alert("Combat system coming soon!")} label={"dodge"}></Button>
-			<Button onClick={() => alert("Combat system coming soon!")} label={"defend"}></Button>
-
+				<p>Choose your action:
+					<span className="combat-controls">
+						<Button onClick={() => alert("Combat system coming soon!")} label={"dash"}></Button>
+						<Button onClick={() => alert("Combat system coming soon!")} label={"dodge"}></Button>
+						<Button onClick={() => alert("Combat system coming soon!")} label={"defend"}></Button>
+					</span>
+				</p>
 			</Conversation>
 			{children}
 		</div>
