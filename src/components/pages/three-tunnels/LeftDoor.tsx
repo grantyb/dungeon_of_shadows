a6d14d4a01cf306fb08414aaa484b35d8dbb2687
Grant Berridge <grant@cactuslab.com>
2026-03-15 16:02:02 +1300

    chore: whitespace

--- src/components/pages/three-tunnels/LeftDoor.tsx
+++ src/components/pages/three-tunnels/LeftDoor.tsx
@@ -9,27 +9,27 @@ export const LeftDoor = () => {
 	return (
 		<StandardPage backgroundImage={img} title="The Plasma beast">
 			<Conversation width={20}>
-					<p>
-						You enter the tunnel on the left.</p>
-					<p>
-						You walk a long way down the mysterious tunnel.
-					</p>
-					<p>
-						You find yourself in a giant room filled with fire,
-						you look up and see a huge mouth seeming to be hungry for your flesh
-					</p>
-					<p>
-						The creature pounces at you,but you manage to dodge its attack
-						in time.
-					</p>
-					<p>
-						It is now your turn to attack
-						<span className="conversation-controls">
-							<button 
-								onClick={() => navigate("/tunnels/plasma/")}>Start turn
-							</button>
-						</span>
-					</p>
+				<p>
+					You enter the tunnel on the left.</p>
+				<p>
+					You walk a long way down the mysterious tunnel.
+				</p>
+				<p>
+					You find yourself in a giant room filled with fire,
+					you look up and see a huge mouth seeming to be hungry for your flesh
+				</p>
+				<p>
+					The creature pounces at you, but you manage to dodge its attack
+					in time.
+				</p>
+				<p>
+					It is now your turn to attack
+					<span className="conversation-controls">
+						<button 
+							onClick={() => navigate("/tunnels/plasma/")}>Start turn
+						</button>
+					</span>
+				</p>
 			</Conversation>
 		</StandardPage>
 	)
